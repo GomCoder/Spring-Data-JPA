2ae009c456883ae015d22433f6476a6f6fc4d690
GomCoder <k2001911@hanmail.net>
2023-01-29 07:47:13 +0900

    JpaRepository 살펴 보기

--- SpringDataJPA/src/main/java/com/fastcampus/jpa/bookmanager/domain/User.java
+++ SpringDataJPA/src/main/java/com/fastcampus/jpa/bookmanager/domain/User.java
@@ -3,6 +3,9 @@ package com.fastcampus.jpa.bookmanager.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
@@ -10,12 +13,21 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 @Data
 @Builder
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
     @NonNull
     private String name; //이름
+
     @NonNull
     private String email; //이메일
+
     private String address; //주소
+
     private LocalDateTime createdAt; //생성 시간
+
     private LocalDateTime updatedAt; //수정 시간
 }
