b78dd85cb9ad2abb2a332419c273ab85cc5500ea
GomCoder <k2001911@hanmail.net>
2023-02-02 14:59:24 +0900

    1:1 연관관계에서의 jpa 활용법

--- SpringDataJPA/src/test/resources/data.sql
+++ SpringDataJPA/src/test/resources/data.sql
@@ -1,14 +1,14 @@
-CALL NEXT VALUE FOR hibernate_sequence;
+--CALL NEXT VALUE FOR hibernate_sequence;
 INSERT INTO USER(`id`, `name`, `email`, `created_at`, `updated_at`) VALUES(1, 'martin', 'martin@fastcampus.com', now(), now());
 
-CALL NEXT VALUE FOR hibernate_sequence;
+--CALL NEXT VALUE FOR hibernate_sequence;
 INSERT INTO USER(`id`, `name`, `email`, `created_at`, `updated_at`) VALUES(2, 'dennis', 'dennis@fastcampus.com', now(), now());
 
-CALL NEXT VALUE FOR hibernate_sequence;
+--CALL NEXT VALUE FOR hibernate_sequence;
 INSERT INTO USER(`id`, `name`, `email`, `created_at`, `updated_at`) VALUES(3, 'sophia', 'sophia@slowcampus.com', now(), now());
 
-CALL NEXT VALUE FOR hibernate_sequence;
+--CALL NEXT VALUE FOR hibernate_sequence;
 INSERT INTO USER(`id`, `name`, `email`, `created_at`, `updated_at`) VALUES(4, 'james', 'james@slowcampus.com', now(), now());
 
-CALL NEXT VALUE FOR hibernate_sequence;
+--CALL NEXT VALUE FOR hibernate_sequence;
 INSERT INTO USER(`id`, `name`, `email`, `created_at`, `updated_at`) VALUES(5, 'martin', 'martin@another.com', now(), now());
