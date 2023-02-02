6929ca98959d999abf0a6904ef581b517d0e1147
GomCoder <k2001911@hanmail.net>
2023-02-02 16:17:22 +0900

    1:N 연관관계에서의 JPA 활용법

--- SpringDataJPA/src/main/java/com/fastcampus/jpa/bookmanager/domain/User.java
+++ SpringDataJPA/src/main/java/com/fastcampus/jpa/bookmanager/domain/User.java
@@ -10,6 +10,8 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -19,7 +21,6 @@ import java.time.LocalDateTime;
 @EqualsAndHashCode(callSuper = true)
 @Builder
 @Entity
-//@Table
 @EntityListeners(value= {UserEntityListener.class})
 public class User extends BaseEntity {
     @Id
@@ -35,62 +36,9 @@ public class User extends BaseEntity {
     @Enumerated(value=EnumType.STRING)
     private Gender gender;
 
-    //@Column(updatable = false)
-    /*@CreatedDate
-    private LocalDateTime createdAt; //생성 시간
-    @LastModifiedDate
-    private LocalDateTime updatedAt; //수정 시간*/
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name="user_id", insertable = false, updatable = false)
+    private List<UserHistory> userHistories = new ArrayList<>();
 
-//    @Transient
-//    private String testData;
 
-    //@OneToMany(fetch = FetchType.EAGER)
-    //private List<Address> addresses; //주소
-
-/*    @PrePersist
-    public void prePersist() {
-        System.out.println(">>> prePersist");
-    }
-
-    @PostPersist
-    public void postPersist() {
-        System.out.println(">>> postPersist");
-    }
-
-    @PreUpdate
-    public void preUpdate() {
-        System.out.println(">>> preUpdate");
-    }
-
-    @PostUpdate
-    public void postUpdate() {
-        System.out.println(">>> postUpdate");
-    }
-
-    @PreRemove
-    public void preRemove() {
-        System.out.println(">>> preRemove");
-    }
-
-    @PostRemove
-    public void postRemove() {
-        System.out.println(">>> postRemove");
-    }
-
-    @PostLoad
-    public void postLoad() {
-        System.out.println(">>> postLoad");
-    }*/
-
-//    @PrePersist
-//    public void prePersist() {
-//        this.createdAt = LocalDateTime.now();
-//        this.updatedAt = LocalDateTime.now();
-//    }
-//
-//    @PostUpdate
-//    public void postUpdate() {
-//        this.updatedAt = LocalDateTime.now();
-//
-//    }
 }
